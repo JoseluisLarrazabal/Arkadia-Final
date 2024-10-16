bcfea6fbd6cc46bcc120b51f09071397cf29920f
Jose Luis Larrazabal <LFJ1009416@est.univalle.edu>
2024-10-16 09:29:28 -0400

    Proyecto base con todos los arreglos

---
+++ Assets/01_Scripts/Player/Weapon.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private BoxCollider2D WeaponCollider;
+
+    public GameObject[] coins;
+
+    void Start()
+    {
+        WeaponCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Orc"))
+        {
+            Destroy(collision.gameObject);
+
+            int i = Random.Range(0, coins.Length);
+            Instantiate(coins[i], transform.position, transform.rotation);
+
+        }
+    }
+}
